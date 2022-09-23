06d0353e728abbdc641ac2deef71a1b52ff2c8f3
Crimson-Hat <98809053+Crimson-Hat@users.noreply.github.com>
2022-09-23 08:19:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,11 @@ mongoose.connection.on("disconnected", () => {
 });
 
 //middlewares
+const corsOptions = {
+  origin: "https://spaceseal.netlify.app",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 app.use(cors())
 app.use(cookieParser())
 app.use(express.json());
